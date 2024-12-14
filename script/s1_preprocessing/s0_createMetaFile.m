5e5cf648cf5f0b7c678f9dddc2aad21f3a450db1
Ricardo Bruña <bru@number5>
2024-12-14 14:06:11 +0100

    Fixed an error when interpreting the "times" file.

--- script/s1_preprocessing/s0_createMetaFile.m
+++ script/s1_preprocessing/s0_createMetaFile.m
@@ -12,7 +12,8 @@ text     = fread ( fid, [ 1 Inf ], '*char' );
 fclose ( fid );
 
 % Adds a new line at the end of the file.
-text     = strcat ( text, newline );
+% text     = strcat ( text, newline );
+text     = cat ( 2, text, newline );
 
 % Replaces the carriage returns by new lines.
 text     = strrep ( text, sprintf ( '\r' ),   newline );
